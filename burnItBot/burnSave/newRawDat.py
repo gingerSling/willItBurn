a45873c6a754e2f02a44ff0b418e323fadfa17a0
papis <pabportillo@gmail.com>
2019-07-22 13:32:36 +0200

    Added a script that bind the post and saves them in the db

--- burnItBot/burnSave/newRawDat.py
+++ burnItBot/burnSave/newRawDat.py
@@ -1,6 +1,8 @@
 import django
 import pandas as pd
 import os
+import datetime
+import numpy as np
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "burnSave.settings") 
 django.setup()
 from polls.models import rawDat, whosHot, whosTop
@@ -16,7 +18,7 @@ def readPosts(dir):
     for i in range(1,len(files)):
         dat=pd.concat([dat,files[i]])
     return dat
-    
+
 def readHot(dir):
     fileName= dir + '/whosHot.csv'
     hot=pd.read_csv(fileName, encoding='utf8')
@@ -26,65 +28,70 @@ def readTop(dir):
     fileName= dir + '/whosTop.csv'
     top=pd.read_csv(fileName, encoding='utf8')
     return top
-
+print("Writing on the DB")
+print(datetime.datetime.now())
 dat=readPosts('../reader')
 dat.rename(columns={'id': 'idP'}, inplace=True)
 dat['id'] = dat['idP'] + '_' + dat['measured'].astype(str)
 dat.drop_duplicates(subset ="id", keep = 'first', inplace = True) 
-df = pd.DataFrame(list(rawDat.objects.all().values('id')))
+df = pd.DataFrame(list(rawDat.objects.all().values('measured')))
+last=float(np.max(df.measured.values))
+dat=dat[dat.measured.values>last]
 if(dat.shape[0]!=0):
-	ids=[item for item in dat.id.values.tolist() if item not in df.id.values.tolist()]
-	dat=dat.loc[dat['id'].isin(ids)]
-	print(dat.shape)
+    print("Data dimensions")
+    print(dat.shape)
 
-	for index, row in dat.iterrows():
-		model = rawDat()
-		model.id = row['id']
-		model.title = row['title']
-		model.author = row['author']
-		model.edited = row['edited']
-		model.is_self = row['is_self']
-		model.locked = row['locked']
-		model.spoiler = row['spoiler']
-		model.stickied = row['stickied']
-		model.score = row['score']
-		model.upvote_ratio = row['upvote_ratio']
-		model.idP = row['idP']
-		model.subreddit = row['subreddit']
-		model.url = row['url']
-		model.num_comments = row['num_comments']
-		model.body = row['body']
-		model.created = row['created']
-		model.measured = row['measured']
-		model.save()
+    for index, row in dat.iterrows():
+        model = rawDat()
+        model.id = row['id']
+        model.title = row['title']
+        model.author = row['author']
+        model.edited = row['edited']
+        model.is_self = row['is_self']
+        model.locked = row['locked']
+        model.spoiler = row['spoiler']
+        model.stickied = row['stickied']
+        model.score = row['score']
+        model.upvote_ratio = row['upvote_ratio']
+        model.idP = row['idP']
+        model.subreddit = row['subreddit']
+        model.url = row['url']
+        model.num_comments = row['num_comments']
+        model.body = row['body']
+        model.created = row['created']
+        model.measured = row['measured']
+        model.save()
     
 
 dat=readHot('../reader/server')
 dat.drop_duplicates(subset ="id", keep = 'first', inplace = True) 
-df = pd.DataFrame(list(whosHot.objects.all().values('id')))
+df = pd.DataFrame(list(whosHot.objects.all().values('time')))
+last=float(np.max(df.time.values))
+dat=dat[dat.time.values>last]
 if(dat.shape[0]!=0 & df.shape[0]!=0):
-	ids=[item for item in dat.id.values.tolist() if item not in df.id.values.tolist()]
-	dat=dat.loc[dat['id'].isin(ids)]
-	print(dat.shape)
+    print(dat.shape)
 if(dat.shape[0]!=0):
-	for index, row in dat.iterrows():
-		model = whosHot()
-		model.id = row['id']
-		model.time = row['time']
-		model.save()
+    print("Hot dimensions")
+    print(dat.shape)
+    for index, row in dat.iterrows():
+        model = whosHot()
+        model.id = row['id']
+        model.time = row['time']
+        model.save()
     
 dat=readTop('../reader/server')
 dat.drop_duplicates(subset ="id", keep = 'first', inplace = True) 
-df = pd.DataFrame(list(whosHot.objects.all().values('id')))
-
+df = pd.DataFrame(list(whosTop.objects.all().values('time')))
+last=float(np.max(df.time.values))
+dat=dat[dat.time.values>last]
 
 if(dat.shape[0]!=0 & df.shape[0]!=0):
-	ids=[item for item in dat.id.values.tolist() if item not in df.id.values.tolist()]
-	dat=dat.loc[dat['id'].isin(ids)]
-	print(dat.shape)
+    print(dat.shape)
 if(dat.shape[0]!=0):
-	for index, row in dat.iterrows():
-		model = whosTop()
-		model.id = row['id']
-		model.time = row['time']
-		model.save()
+    print("Top dimensions")
+    print(dat.shape)
+    for index, row in dat.iterrows():
+        model = whosTop()
+        model.id = row['id']
+        model.time = row['time']
+        model.save()
